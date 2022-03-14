7f591528b44985f08e40cbde7122e95f454601d2
Carlos Florez <cflorez@orocash.ec>
2022-03-14 00:48:40 -0500

    Se separan las funciones para cifrar y descifrar, se limpia repositorio

--- db/script.sql
+++
@@ -1,7 +0,0 @@
-create table tb_cuenta (
-	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	platform varchar(50),
-	username varchar(25),
-	password varchar(50),
-	create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
